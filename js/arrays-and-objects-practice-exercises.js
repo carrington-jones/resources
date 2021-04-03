2d39f02c0f2ac35dced1b73d38bd7ac7b3e9f74e
Carrington Jones <carrington.jones47@gmail.com>
2021-04-03 13:34:12 -0500

    updated with completed exercise # 2

--- js/arrays-and-objects-practice-exercises.js
+++ js/arrays-and-objects-practice-exercises.js
@@ -1,4 +1,6 @@
 "use strict"
+
+//1.)
 // Write a function, filterNumbers() that takes in an array of mixed data types and returns an array of only the numbers type in ascencding order.
 
 
@@ -22,15 +24,30 @@ function filterNumber (array) {
 }
 console.log(filterNumber(["fred", true, "banana", 5, 3, 7, 1000]));
 
+// 2.)
+// Write a function, getOlder() that takes in array of dog objects and increases the value of each object's age property by 1.
 
-
-var ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-for( var i = 0; i < ar.length; i++){
-
-    if ( ar[i] === 5) {
-
-        ar.splice(i, 1);
+var dogs = [
+    {
+        name: "Chompers",
+        breed: "Pug",
+        age: 7
+    },
+    {
+        name: "Freddy",
+        breed: "Lab",
+        age: 4
+    },
+    {
+        name: "Mr. Pig",
+        breed: "Mastif",
+        age: 10
     }
+]
 
-} console.log(ar);
+function getOlder(array){
+    dogs.forEach(function (dog){
+        dog.age++;
+    }); return dogs
+}
+console.log(getOlder(dogs));
