49f9f83e22669f6a65332236814a098ae3d1bfb2
Carrington Jones <carrington.jones47@gmail.com>
2021-04-24 12:34:36 -0500

    Worked on Codeup arrays extra exercises

--- js/arrays-practice-exercses.js
+++ js/arrays-practice-exercses.js
@@ -73,6 +73,7 @@ function rollDie () {
 console.log(rollDie());
 
 // Make a function called listOfRolls(num) that takes in a number containing how many 6-sided dice rolls you want to make. The listOfRolls function should return an array of that length, where each element of the array is the result of the rollDie function.
+
 var numberOfRolls = []
 function listOfRolls (num) {
     for( var i = 0; i < num; i++) {
@@ -80,6 +81,8 @@ function listOfRolls (num) {
     } return numberOfRolls
 }
 console.log(listOfRolls(3));
+
+// Make a function called listOfRollsFromDieFunc(numberOfRolls, diceFunction)
 
 // function listOfRollsFromDieFunc(numberOfRolls,diceFunction) {
 //     var rollsArray = []
@@ -90,15 +93,126 @@ console.log(listOfRolls(3));
 //     return rollsArray;
 // }
 
-function listOfRollsFromDieFunc(numOfRolls, diceFunc){
-    var rollsArray = []
-    for(var i = 1; i <= numOfRolls; i++){
-        var d = diceFunc
-        rollsArray.push(d);
-    }
-    return rollsArray
+// function listOfRollsFromDieFunc(numOfRolls, diceFunc){
+//     var rollsArray = []
+//     for(var i = 1; i <= numOfRolls; i++){
+//         var d = diceFunc
+//         rollsArray.push(d);
+//     }
+//     return rollsArray
+// }
+//
+// console.log(listOfRollsFromDieFunc(4, rollDie()));
+// console.log(listOfRollsFromDieFunc(5, rollDie()));
+// console.log(listOfRollsFromDieFunc(3, rollDie()));
+
+// Exercise 0. Write a function named first() that returns only the first element of an array
+
+// var sampleArray = [1, 2, 3, 4, 5]
+// function first (input){
+//     return [input[0]]
+// }
+// console.log(first(sampleArray));
+
+// Exercise 1. Write a function named secondToLast() that returns the second to last element
+// var sampleArray = [1, 2, 3, 4, 5]
+// function secondToLast(input){
+//     input.shift();
+//     return input
+// }
+// console.log(secondToLast(sampleArray))
+
+// Exercise 2. Write a function named rest() that takes an an array and returns an array containing everything except the first element.
+// var sampleArray = [1, 2, 3, 4, 5]
+// function secondToLast(input){
+//     input.shift();
+//     return input
+// }
+// console.log(secondToLast(sampleArray))
+
+// Exercise 3. Write a function named getLongestString that takes in an array of strings and returns the longest string of that array
+// var arr = [
+//     'first item',
+//     'second item is longer than the third one',
+//     'third longish item'
+// ];
+// function getLongestString(input) {
+//     var lgth = 0;
+//     var longest;
+//     for (var i = 0; i < input.length; i++) {
+//         if (input[i].length > lgth) {
+//             var lgth = input[i].length;
+//             longest = input[i];
+//         }
+//     } return longest
+// }
+// console.log(getLongestString(arr));
+
+// Exercise 3.1 Write a function named getShortestString that takes in an array of strings and returns the shortest string in that array.
+var arr = [
+    'first item',
+    'second item is longer than the third one',
+    'third longish item'
+
+];
+function getShortestString(input) {
+    var lgth = Infinity;
+    var shortest;
+    for (var i = 0; i < input.length; i++) {
+        if (input[i].length < lgth) {
+            shortest = input[i];
+            var lgth = input[i].length; //This causes shortest string to become "new infinity"
+        }
+    } return shortest
+}
+console.log(getShortestString(arr));
+
+// Exercise 4. Write a function named addTwoArrays that takes in two, one dimensional arrays. The function should return a single array containing all of the elements of the first array along with all of the elements of the second array
+// Example: addTwoArrays([1, 2, 3], [4, 5, 6]) should return [1, 2, 3, 4, 5, 6]
+function addTwoArrays (array1, array2){
+    for (var i = 0; i < array2.length; i++ ) {
+        array1.push(array2[i]);
+    } return array1
+}
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+
+console.log(addTwoArrays(arr1, arr2));
+
+// Exercise 5. Write a function named getUniqueValues that takes in an array and returns the array without any duplicates
+// Example: getUniqueValues(["a", "b", "a", "b", "c", "c"]) should return ["a", "b", "c"]
+function getUniqueValues (input) {
+
 }
 
-console.log(listOfRollsFromDieFunc(4, rollDie()));
-console.log(listOfRollsFromDieFunc(5, rollDie()));
-console.log(listOfRollsFromDieFunc(3, rollDie()));
+// Exercise 6. Write a function named reverseArray that takes in an array and returns it reversed, but without altering the original array.
+var sampleArray = ["a", "b", "c"];
+
+function reverseArray (input) {
+    var newArray = input.slice().reverse();
+    return newArray;
+}
+console.log(reverseArray(sampleArray));
+console.log(sampleArray);
+
+// Exercies 7. Write a function named getRandomQuote().
+//   Inside of the function, create an array of strings where each string is a quote or thought you find inspirational
+//   getRandomQuote should generate a random number between 0 and the array's length minus 1
+//   use the randomly generated number as your index
+//   return a random quote.
+function getRandomQuote(input){
+    var quotes = [
+        "yes",
+        "no",
+        "maybe",
+        "possibly"
+    ]
+    return quotes[(Math.floor(Math.random() * quotes.length) + 1)];
+}
+console.log(getRandomQuote());
+// Exercise 8. Write a function named getIndexesOf() that takes in two arguments.
+// The first argument should be a specific numeral or character
+// The second argument should be any given string
+// getIndexesOf() should return an array containing all of the indexes of that character in the string
+// Example: getIndexesOf("a", "banana") should return the array [1, 3, 5]
+// Example: getIndexesOf("z", "banana") should return an empty array [] since there are no "z" characters in "banana"
